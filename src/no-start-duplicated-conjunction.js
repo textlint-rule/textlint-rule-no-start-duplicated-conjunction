dff05c5e85230cfaa5f2f3a4c3df3858861a2500
azu <azuciao@gmail.com>
2017-12-24 22:27:23 +0900

    fix(rule): upgrade to sentence-splitter

--- src/no-start-duplicated-conjunction.js
+++ src/no-start-duplicated-conjunction.js
@@ -1,54 +1,70 @@
 // LICENSE : MIT
 "use strict";
-import {RuleHelper, IgnoreNodeManager} from "textlint-rule-helper";
+import { RuleHelper, IgnoreNodeManager } from "textlint-rule-helper";
 import ObjectAssign from "object-assign";
-const splitSentence = require("sentence-splitter").split;
+
+const splitAST = require("sentence-splitter").splitAST;
 const SentenceSyntax = require("sentence-splitter").Syntax;
-import StringSource from "textlint-util-to-string";
+
 const defaultOptions = {
     interval: 2
 };
 const pointing = /[、,]/;
+
 // conjunction
-function getFirstPhrase(sentence) {
-    var phrases = sentence.value.split(pointing);
+/**
+ * get first node value
+ * @param node
+ * @returns {string|undefined}
+ */
+function getFirstPhrase(node) {
+    const phrases = node.value.split(pointing);
     if (phrases.length > 0) {
         return phrases[0].trim();
     }
 }
+
 module.exports = function(context, options = {}) {
     options = ObjectAssign({}, defaultOptions, options);
     const helper = new RuleHelper(context);
     const ignoreNodeManager = new IgnoreNodeManager();
-    const {Syntax, getSource, report, RuleError} = context;
+    const { Syntax, getSource, report, RuleError } = context;
     let previousPhases = [];
     let useDuplicatedPhase = false;
     return {
         // reset count
-        [Syntax.Header](){
+        [Syntax.Header]() {
             previousPhases = [];
         },
-        [Syntax.HorizontalRule](){
-            previousPhases = []
+        [Syntax.HorizontalRule]() {
+            previousPhases = [];
         },
-        [Syntax.Paragraph](node){
+        [Syntax.Paragraph](node) {
             // FIXME: linkReference should be defined in TxtAST.
             const ignoreTypes = [
-                Syntax.Code, Syntax.Link, "linkReference", Syntax.Image, Syntax.BlockQuote, Syntax.Emphasis
+                Syntax.Code,
+                Syntax.Link,
+                "linkReference",
+                Syntax.Image,
+                Syntax.BlockQuote,
+                Syntax.Emphasis
             ];
             if (helper.isChildNode(node, ignoreTypes)) {
                 return;
             }
             ignoreNodeManager.ignoreChildrenByTypes(node, ignoreTypes);
-            const source = new StringSource(node);
-            const text = source.toString();
-            const sentences = splitSentence(text, {
-                charRegExp: /[。]/
-            }).filter(sentence => {
+            const sentences = splitAST(node).children.filter(sentence => {
                 return sentence.type === SentenceSyntax.Sentence;
             });
             sentences.forEach(sentence => {
-                const phrase = getFirstPhrase(sentence);
+                const firstChild = sentence.children[0];
+                if (!firstChild) {
+                    return;
+                }
+                if (firstChild.type !== Syntax.Str) {
+                    return;
+                }
+                const phrase = getFirstPhrase(firstChild);
                 if (phrase.length === 0) {
                     return;
                 }
@@ -56,17 +72,11 @@ module.exports = function(context, options = {}) {
                     useDuplicatedPhase = true;
                 }
                 if (useDuplicatedPhase) {
-                    const sentenceStartIndex = node.range[0] + sentence.range[0];
-                    const originalIndex = source.originalIndexFromIndex(sentenceStartIndex);
-                    // adjust index 
+                    // adjust index
                     // if  "また、[import, a.js](a.js)" then originalIndex is used.
                     // if "[import, binary-example.js](src/binary-example.js)" then  originalIndex is undefined.
-                    const targetIndex = typeof originalIndex !== "undefined" ? originalIndex : sentenceStartIndex;
-                    if (!ignoreNodeManager.isIgnoredIndex(targetIndex)) {
-                        report(node, new RuleError(`Don't repeat "${phrase}" in ${options.interval} phrases`, {
-                            line: Math.max(sentence.loc.start.line - 1, 0),
-                            column: sentence.loc.start.column
-                        }));
+                    if (!ignoreNodeManager.isIgnored(firstChild)) {
+                        report(firstChild, new RuleError(`Don't repeat "${phrase}" in ${options.interval} phrases`));
                     }
                     useDuplicatedPhase = false;
                 }
@@ -75,5 +85,5 @@ module.exports = function(context, options = {}) {
                 previousPhases = previousPhases.slice(0, options.interval);
             });
         }
-    }
+    };
 };
